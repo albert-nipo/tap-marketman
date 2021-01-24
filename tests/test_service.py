ae9eabf7320ab76a2f7e63a5e92640e121d85f76
iEv0lv3 <jordan.wallace.williams@gmail.com>
2021-01-24 01:55:18 -0700

    Update imports and import calls. Call functions from modules: module.function()

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,19 +1,20 @@
 import pytest
-from tap_marketman.service import *
+import requests
+import tap_marketman.service as service
 
 def test_environment_variables():
-    assert isinstance(APIKey, str)
-    assert isinstance(APIPassword, str)
+    assert isinstance(service.APIKey, str)
+    assert isinstance(service.APIPassword, str)
 
 
 def test_create_session():
-    client = create_session()
+    client = service.create_session()
 
     assert isinstance(client, requests.sessions.Session)
 
 
 @pytest.mark.vcr()
 def test_get_token():
-    token = get_token()
+    token = service.get_token()
 
     assert isinstance(token, str)
