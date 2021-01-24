63d4705afbeef7400d572a07250c3c9de81ef86f
iEv0lv3 <jordan.wallace.williams@gmail.com>
2021-01-24 01:54:08 -0700

    Update imports and import calls. Call functions from modules: module.function()

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,24 +1,25 @@
 import pytest
 import requests
-from tap_marketman.endpoints import *
+import tap_marketman.endpoints as endpoints
+import tap_marketman.service as service
 
 
 def test_create_session():
-    client = create_session()
+    client = service.create_session()
 
     assert isinstance(client, requests.sessions.Session)
 
 
 @pytest.mark.vcr()
 def test_get_token():
-    token = get_token()
+    token = service.get_token()
 
     assert isinstance(token, str)
 
 
 @pytest.mark.vcr()
 def test_get_guid():
-    guid_list = get_guid()
+    guid_list = endpoints.get_guid()
 
     assert isinstance(guid_list['Buyers'], list)
     assert isinstance(guid_list['Vendors'], list)
