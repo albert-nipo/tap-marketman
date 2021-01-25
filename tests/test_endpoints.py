455d2338a27b6d75a880de396b31c805f91fb822
Jordan <jordan.wallace.williams@gmail.com>
2021-01-25 13:55:20 -0700

    Add test_get_inventory_items()

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -24,3 +24,15 @@ def test_get_guid():
     assert isinstance(guid_list['Buyers'], list)
     assert isinstance(guid_list['Vendors'], list)
     assert isinstance(guid_list['Chains'], list)
+
+
+@pytest.mark.vcr()
+def test_get_inventory_items():
+    guid = "9a8f1b1c7ee94eb3a29be4f9a2939987"
+    inventory_items = endpoints.get_inventory_items(guid)
+
+    assert isinstance(inventory_items, list)
+    assert isinstance(inventory_items[0], dict)
+    assert isinstance(inventory_items[0]['Name'], str)
+    assert isinstance(inventory_items[0]['ID'], str)
+
